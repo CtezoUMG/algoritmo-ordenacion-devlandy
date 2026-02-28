f27c4870bba86bafd501798a9a446ac0dad6f1c8
Landy Cux <lcuxr@miumg.edu.gt>
2026-02-28 08:31:22 -0600

    implementacion bubblesort

--- Program.cs
+++ Program.cs
@@ -11,6 +11,24 @@ Console.WriteLine($"Procesando: {CANTIDAD_ELEMENTOS:N0} números.");
 
 // 2. El alumno implementa esto en la clase Algoritmo
 ordenador.BubbleSort(datos);
+{
+        int n = arreglo.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                // Si el elemento actual es mayor al siguiente, se intercambian
+                if (arreglo[j] > arreglo[j + 1])
+                {
+                    // Intercambio (Swap)
+                    int temporal = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = temporal;
+                }
+            }
+        }
+    }
+
 
 // 3. Validación de integridad
 if (ordenador.EstaOrdenado(datos))
